3f781abfecb6e1d4ecf216b07299f6e7b89330d4
BuildTools <unconfigured@null.spigotmc.org>
2022-06-07 14:30:42 -0500

    updates to quick_plot_tel_composition

--- quick_plot_tel_composition.py
+++ quick_plot_tel_composition.py
@@ -19,15 +19,17 @@ DUMMY_DBTA = 0
 
 def main(raw_args=None):
 	parser = argparse.ArgumentParser(description='quick_plot_tel_composition.py', formatter_class=argparse.ArgumentDefaultsHelpFormatter,)
-	parser.add_argument('-i',  type=str, required=True,              metavar='* input.sam',    help="* Input SAM or FASTA")
-	parser.add_argument('-o',  type=str, required=True,              metavar='* output.png',   help="* Output plot")
-	parser.add_argument('-k',  type=str, required=False, default='', metavar='plot_kmers.tsv', help="Telomere kmers to use for composition plotting")
+	parser.add_argument('-i', type=str, required=True,                 metavar='* input.sam',    help="* Input SAM or FASTA")
+	parser.add_argument('-o', type=str, required=True,                 metavar='* output.png',   help="* Output plot")
+	parser.add_argument('-k', type=str, required=False, default='',    metavar='plot_kmers.tsv', help="Telomere kmers to use for composition plotting")
+	parser.add_argument('-s', type=str, required=False, default='fwd', metavar='fwd/rev/both',   help="Telomere kmer orientation")
 	args = parser.parse_args()
 
 	IN_SAM   = args.i
 	OUT_PLOT = args.o
 
 	KMER_FILE   = args.k
+	KMER_STRAND = args.s
 	KMER_LIST   = []
 	KMER_COLORS = []
 	rev_kmers   = []
@@ -38,37 +40,43 @@ def main(raw_args=None):
 		kmer_fn  = str(sim_path) + '/resources/plot_kmers.tsv'
 		f = open(kmer_fn,'r')
 		for line in f:
-			splt = line.strip().split('\t')
-			KMER_LIST.append(splt[1])
-			KMER_COLORS.append(splt[2])
-			rev_kmers.append(RC(splt[1]))
-			rev_colors.append(splt[2])
+			if line[0] != '#':
+				splt = line.strip().split('\t')
+				KMER_LIST.append(splt[1])
+				KMER_COLORS.append(splt[2])
+				rev_kmers.append(RC(splt[1]))
+				rev_colors.append(splt[2])
 		f.close()
-		KMER_LIST.extend([n for n in rev_kmers])
-		KMER_COLORS.extend([n for n in rev_colors])
 	else:
 		fn_suffix = KMER_FILE.split('/')[-1]
 		print('using user-specified kmer list:', fn_suffix)
 		if exists_and_is_nonzero(KMER_FILE):
 			f = open(KMER_FILE,'r')
 			for line in f:
-				splt = line.strip().split('\t')
-				KMER_LIST.append(splt[1])
-				KMER_COLORS.append(splt[2])
-				rev_kmers.append(RC(splt[1]))
-				rev_colors.append(splt[2])
+				if line[0] != '#':
+					splt = line.strip().split('\t')
+					KMER_LIST.append(splt[1])
+					KMER_COLORS.append(splt[2])
+					rev_kmers.append(RC(splt[1]))
+					rev_colors.append(splt[2])
 			f.close()
-			KMER_LIST.extend([n for n in rev_kmers])
-			KMER_COLORS.extend([n for n in rev_colors])
 		else:
 			print('Error: kmer list not found')
 			exit(1)
-	sorted_kmer_dat  = sorted([(len(KMER_LIST[n]),KMER_LIST[n],KMER_COLORS[n]) for n in range(len(KMER_LIST))], reverse=True)	# sort by length
+	if KMER_STRAND in ['fwd', 'rev']:
+		sorted_kmer_dat = sorted(list(set([(len(KMER_LIST[n]),KMER_LIST[n],KMER_COLORS[n]) for n in range(len(KMER_LIST))])), reverse=True)
+	elif KMER_STRAND == 'both':
+		sorted_kmer_dat = sorted(list(set([(len(KMER_LIST[n]),KMER_LIST[n],KMER_COLORS[n]) for n in range(len(KMER_LIST))]+[(len(KMER_LIST[n]),RC(KMER_LIST[n]),KMER_COLORS[n]) for n in range(len(KMER_LIST))])), reverse=True)
+	else:
+		print('Error: -s must be fwd, rev, or both')
 	KMER_LIST        = [n[1] for n in sorted_kmer_dat]
 	KMER_COLORS      = [n[2] for n in sorted_kmer_dat]
 	KMER_ISSUBSTRING = []
 	for i in range(len(KMER_LIST)):
 		KMER_ISSUBSTRING.append([j for j in range(len(KMER_LIST)) if (j != i and KMER_LIST[i] in KMER_LIST[j])])
+	
+	if KMER_STRAND == 'rev':
+		KMER_LIST = [RC(n) for n in KMER_LIST]
 	
 	#
 	#
@@ -90,7 +98,8 @@ def main(raw_args=None):
 			read_dat.append([my_name, len(my_rdat), my_rdat, DUMMY_MAPQ])
 		my_reader.close()
 	#
-	my_chr       = 'placeholder-title q'
+	my_chr       = 'None-q'
+	my_pos       = 'None'
 	my_type      = 'q'
 	kmer_hit_dat = []
 	line_num     = 0
@@ -120,7 +129,7 @@ def main(raw_args=None):
 		#
 		kmer_hit_dat.append([get_nonoverlapping_kmer_hits(my_telseq, KMER_LIST, KMER_ISSUBSTRING), my_tlen, DUMMY_DBTA, my_type, my_rnm, my_mapq])	# kmer_hit_dat[-1][0][ki] = hits in current read for kmer ki
 	#
-	plot_kmer_hits(kmer_hit_dat, KMER_COLORS, my_chr, OUT_PLOT)
+	plot_kmer_hits(kmer_hit_dat, KMER_COLORS, my_chr, my_pos, OUT_PLOT)
 	#
 
 if __name__ == '__main__':
